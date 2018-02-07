98aaaaec940a38c1dbcddaa0f3e8b68439e7b104
Bit <Bit@Bitcamp>
2018-02-07 21:43:49 +0900

    채윤작업

--- myBatisTest/src/org/kdea/mybatis/EmpMBController.java
+++ myBatisTest/src/org/kdea/mybatis/EmpMBController.java
@@ -20,9 +20,7 @@ public class EmpMBController {
 
 	@RequestMapping("/empList")
 	public String getEmpList(Model model) {
-		System.out.println("리스트로가기전에 테스트를 거친다.");
-		System.out.println("출근시간이다가오는 kdy");
-		System.out.println("집갈시간이온다");
+		System.out.println("채윤누나작업중");
 		EmpDAO dao = sqlSessionTemplate.getMapper(EmpDAO.class);
 		List<Emp> list = dao.list();
 		model.addAttribute("empList", list);
