7d2960d18ddebd905b0858a1b8e410c5b72231e9
Bit <Bit@Bitcamp>
2018-02-08 11:19:15 +0900

    김채윤 11시 19분

--- myBatisTest/src/org/kdea/mybatis/PushTest.java
+++ myBatisTest/src/org/kdea/mybatis/PushTest.java
@@ -6,11 +6,18 @@ public class PushTest {
 		System.out.println("10시 48분");
 		System.out.println("11시 12분");
 		
+		Emp emp = new Emp();
 		
+		emp.setDeptno(1);
+		
+		System.out.println(emp.getDeptno());
 		
 		
 		
 		System.out.println("11시 02분");
 		System.out.println("11시 09분");
+		
+		
+		System.out.println( "11시 13분 작업중");
 	}
 }
